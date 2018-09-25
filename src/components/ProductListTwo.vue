d0f5da4ac54aa63c6ee844f615d490dfa7e1c083
FravashY <fravashy@gmail.com>
2018-09-25 23:23:31 +0300

    [+] Tutorial #5 - Getters

--- src/components/ProductListTwo.vue
+++ src/components/ProductListTwo.vue
@@ -2,7 +2,7 @@
     <div id="product-list-two">
       <h2>Product List Two</h2>
       <ul>
-        <li v-for="product in products">
+        <li v-for="product in saleProducts">
           <span class="name">{{ product.name }}</span>
           <span class="price">${{ product.price }}</span>
         </li>
@@ -22,6 +22,9 @@
     computed: {
       products(){
         return this.$store.state.products
+      },
+      saleProducts(){
+        return this.$store.getters.saleProducts
       }
     }
   }
